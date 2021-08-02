e437539e5318a55b51f0dd62c7d73f723eb3a22d
Thomas <thombsaway@gmail.com>
2021-08-02 13:14:39 +1000

    Add call to test API.

--- web/lift/routes/index.py
+++ web/lift/routes/index.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, flash, g, redirect, render_template, request, session, url_for, g
+import requests
 
 bp = Blueprint('', __name__)
 
@@ -6,4 +7,7 @@ bp = Blueprint('', __name__)
 @bp.route('/index')
 def index():
 
-    return render_template('index.html')
+    response = requests.get("https://qnzfldut9f.execute-api.us-east-1.amazonaws.com/dev/")
+    message = response.json()['message']
+
+    return render_template('index.html', message=message)
