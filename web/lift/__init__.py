1649c86fd69da53d09cb9d8bb631f9ee0c2002eb
Thomas <thombsaway@gmail.com>
2021-08-04 19:59:07 +1000

    register bluprints

--- web/lift/__init__.py
+++ web/lift/__init__.py
@@ -7,7 +7,9 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(SECRET_KEY='dev')
 
-    from .routes import index
+    from .routes import index, session, exercise
     app.register_blueprint(index.bp)  
+    app.register_blueprint(session.bp)
+    app.register_blueprint(exercise.bp)
 
     return app
