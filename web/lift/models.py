e340c44986c1344fd1fdd6f76784d2244be37e5a
Thomas <thombsaway@gmail.com>
2021-08-17 14:30:37 +1000

    Update user model

--- web/lift/models.py
+++ web/lift/models.py
@@ -1,10 +1,13 @@
+
 from flask_login import UserMixin
 
 class User(UserMixin):
 
-    def __init__(self, id, email, password, sessions, coach=False):
+    def __init__(self, id, email, name, weightclass, password, sessions, coach=False):
         self.id = id
         self.email = email
         self.password = password
+        self.name = name
+        self.weightclass = weightclass
         self.sessions = sessions
         self.coach = coach
