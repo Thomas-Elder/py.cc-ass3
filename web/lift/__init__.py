04ed16c619018e9d073d95ce338beb2e46dc3011
Thomas <thombsaway@gmail.com>
2021-08-17 12:41:25 +1000

    Update route rego, and add loginmanager init

--- web/lift/__init__.py
+++ web/lift/__init__.py
@@ -1,15 +1,23 @@
 import os
 
 from flask import Flask
+from flask_login import login_manager, login_user, logout_user, login_required, current_user, LoginManager
 
 def create_application(test_config=None):
     # create and configure the application
     application = Flask(__name__, instance_relative_config=True)
     application.config.from_mapping(SECRET_KEY='dev')
 
-    from .routes import index, session, exercise
+    from .routes import index
+    from .routes import athlete, coach
     application.register_blueprint(index.bp)  
-    application.register_blueprint(session.bp)
-    application.register_blueprint(exercise.bp)
+    application.register_blueprint(athlete.bp)
+    application.register_blueprint(coach.bp)
+
+    loginManager = LoginManager(application)
+    @loginManager.user_loader
+    def load_user(id):
+        # set up get_user function somewhere, or maybe just call API here?
+        pass
 
     return application
