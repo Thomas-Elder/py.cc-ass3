a021208aecf1dd1f816fc2a3b26ad8ab20b9f67f
Thomas <thombsaway@gmail.com>
2021-08-18 21:28:50 +1000

    Remove load_user, just using get_user

--- web/lift/api.py
+++ web/lift/api.py
@@ -10,31 +10,25 @@ API_user = API_STRING + "user"
 API_athlete = API_STRING + "athlete"
 API_coach = API_STRING + "coach"
 
-def load_user(id):
-    """
-    load_user
-    Gets the user details from the API, for use by flask_login for tracking current_user.
-    """
-    response = requests.get(API_user + f"?Email={id}")
-
-    result = response.json()['result']
-    return User(result['Email'], result['Email'], result['Password'], result['Coach'])
-
 def get_user(id):
     """
     load_user
-    Gets the user details from the API, for use by flask_login for tracking current_user.
+    Gets the user details from the API.
     """
-    response = requests.get(API_user + f"?Email={id}")
 
+    response = requests.get(API_user + f"?Email={id}")
     result = response.json()['result']
-    return User(result['Email'], result['Email'], result['Password'], result['Coach'])
+    
+    if result is None:
+        return None
+    else:
+        return User(result['Email'], result['Email'], result['Password'], result['Coach'])
 
 
 def put_user(user: User):
     """
-    load_user
-    Gets the user details from the API, for use by flask_login for tracking current_user.
+    put_user
+    Puts the user details from the API.
     """
     json_user = {
         "Email": user.email,
@@ -43,7 +37,6 @@ def put_user(user: User):
     }
 
     response = requests.put(API_user, json=json_user)
-
     return response
 
 def check_email_unique(email: str) -> bool:
@@ -62,7 +55,7 @@ def check_password(email: str, password: str) -> bool:
 
     Returns true if the password stored with the email matches.
     """
-    user = get_user(email=email)
+    user = get_user(email)
     
     if user is None:
         return False
@@ -78,7 +71,7 @@ def get_athlete(id: str):
     response = requests.get(API_athlete + f"?Email={id}")
 
     result = response.json()['result']
-    return Athlete(result['Email'], result['Email'], result['Name'], result['Age'], result['Weightclass'], result['Sessions'])
+    return Athlete(result['Email'], result['Email'], result['Name'], result['Age'], result['WeightClass'], result['Sessions'])
 
 
 def put_athlete(athlete: Athlete):
@@ -88,14 +81,13 @@ def put_athlete(athlete: Athlete):
     """
     json_athlete = {
         "Email": athlete.email,
-        "Password": athlete.password,
         "Name": athlete.name,
         "Age": athlete.age,
-        "Weightclass": athlete.weightclass,
+        "WeightClass": athlete.weightclass,
         "Sessions": athlete.sessions
     }
 
-    response = requests.put(API_athlete, json_athlete)
+    response = requests.put(API_athlete, json=json_athlete)
     return response
 
 def get_coach(id: str):
