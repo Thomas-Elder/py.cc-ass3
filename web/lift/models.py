182a79b90fc95d9c25bcbf40dd4214c629982a8f
Thomas <thombsaway@gmail.com>
2021-08-18 20:25:56 +1000

    Add models for athletes and coaches

--- web/lift/models.py
+++ web/lift/models.py
@@ -1,14 +1,30 @@
 
+from re import A
 from flask_login import UserMixin
 
 class User(UserMixin):
 
-    def __init__(self, id, email, password, name, weightclass=None, sessions=None, coach=False):
+    def __init__(self, id, email, password, coach):
         self.id = id
         self.email = email
         self.password = password
-        self.name = name
-        self.weightclass = weightclass
-        self.sessions = sessions
         self.coach = coach
 
+class Athlete():
+
+    def __init__(self, email, name, age, weightclass, sessions=[]):
+        self.email = email
+        self.name = name
+        self.age = age
+        self.weightclass = weightclass
+        
+        self.sessions = sessions
+
+class Coach():
+
+    def __init__(self, email, name, athletes=[]):
+        self.email = email
+        self.name = name
+        
+        self.athletes = athletes
+    
