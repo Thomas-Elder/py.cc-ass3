575b1b3e58d7557650b978ac92cdc6c0298990a4
Thomas <thombsaway@gmail.com>
2021-08-18 20:26:44 +1000

    Add forms for registering and logging in

---
+++ web/lift/routes/forms.py
@@ -0,0 +1,62 @@
+from boto3.docs.docstring import AttributeDocstring
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, TextAreaField, PasswordField
+from flask_wtf.file import FileField, FileRequired
+from wtforms.validators import DataRequired
+
+from ..api import check_email_unique, check_password
+
+class RegisterAthleteForm(FlaskForm):
+    email = StringField('email', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired()])
+    name = StringField('name', validators=[DataRequired()])
+    age = StringField('age', validators=[DataRequired()])
+    weightclass = StringField('weightclass', validators=[DataRequired()])  
+    submit = SubmitField('register')
+
+    def validate(self):
+
+        if not FlaskForm.validate(self):
+            return False
+        else:
+
+            if not check_email_unique(self.email.data):
+                self.email.errors.append('The email already exists')
+                return False
+
+            return True
+
+class RegisterCoachForm(FlaskForm):
+    email = StringField('email', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired()])
+    name = StringField('name', validators=[DataRequired()]) 
+    submit = SubmitField('register')
+
+    def validate(self):
+
+        if not FlaskForm.validate(self):
+            return False
+        else:
+
+            if not check_email_unique(self.email.data):
+                self.email.errors.append('The email already exists')
+                return False
+
+            return True
+
+class LoginForm(FlaskForm):
+    email = StringField('email', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired()])
+    submit = SubmitField('login')
+
+    def validate(self):
+
+        if not FlaskForm.validate(self):
+            return False
+        else:
+
+            if check_password(self.email.data, self.password.data):
+                return True
+
+            self.password.errors.append('email or password is invalid')
+            return False
