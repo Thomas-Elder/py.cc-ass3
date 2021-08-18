26b943e72e653ebe6c64d5845c904aa71ae82c9f
Thomas <thombsaway@gmail.com>
2021-08-18 21:29:26 +1000

    add flask login and decorators

--- web/lift/routes/coach.py
+++ web/lift/routes/coach.py
@@ -1,14 +1,17 @@
 from flask import Blueprint, flash, g, redirect, render_template, request, session, url_for, g
 import requests
+from flask_login import login_user, logout_user, login_required, current_user
 
 bp = Blueprint('coach', __name__)
 
+@login_required
 @bp.route('/coach/athletes')
 def athletes():
 
-    return render_template('coach/athletes.html')
+    return render_template('coach/athletes.html', current_user=current_user)
 
+@login_required
 @bp.route('/coach/sessions')
 def sessions():
 
-    return render_template('coach/sessions.html')
+    return render_template('coach/sessions.html', current_user=current_user)
