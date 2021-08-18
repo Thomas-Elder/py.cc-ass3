bbefb6b10bc00adbbd3208fe4a765b0a2e4bbdf4
Thomas <thombsaway@gmail.com>
2021-08-18 20:25:09 +1000

    Removed testing api from index

--- web/lift/routes/index.py
+++ web/lift/routes/index.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, flash, g, redirect, render_template, request, session, url_for, g
 import requests
-from ..api import get_athlete, get_coach
+from ..api import get_athlete, get_coach, put_user, get_user
+
+from ..models import User
 
 bp = Blueprint('', __name__)
 
@@ -8,5 +10,4 @@ bp = Blueprint('', __name__)
 @bp.route('/index')
 def index():
 
-    message = get_athlete("test@gmail.com").email
-    return render_template('index.html', message=message)
+    return render_template('index.html')
