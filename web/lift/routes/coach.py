cf5f7ba4938b447f4ffdcf38892178052ccc47e4
Thomas <thombsaway@gmail.com>
2021-08-19 16:35:18 +1000

    Update coach athlete route

--- web/lift/routes/coach.py
+++ web/lift/routes/coach.py
@@ -8,18 +8,14 @@ bp = Blueprint('coach', __name__)
 @login_required
 @bp.route('/coach/athletes')
 def athletes():
-    
+
     all_athletes = get_athletes()
-    #coach_athlete_emails = get_coach(current_user.id).athletes
     coach = get_coach(current_user.id)
+    
     coach_athletes = filter(lambda athlete: athlete.email in coach.athletes, all_athletes)
-    #for email in coach_athlete_emails:
-    #    coach_athletes.append(get_athlete(email))
+    coachless_athletes = filter(lambda athlete: athlete.coach is None, all_athletes)
 
-    
-    
-
-    return render_template('coach/athletes.html', current_user=current_user, all_athletes=all_athletes,coach_athletes=coach_athletes)
+    return render_template('coach/athletes.html', current_user=current_user, coachless_athletes=coachless_athletes,coach_athletes=coach_athletes)
 
 @login_required
 @bp.route('/coach/sessions')
