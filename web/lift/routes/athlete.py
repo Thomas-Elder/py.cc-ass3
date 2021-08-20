eea44529fa64779ff6752249c6e236724cdb6e0e
Thomas <thombsaway@gmail.com>
2021-08-20 16:30:31 +1000

    Split session and add session apart.

--- web/lift/routes/athlete.py
+++ web/lift/routes/athlete.py
@@ -1,6 +1,7 @@
-from web.lift.api import put_session
+from ..api import get_athlete, get_sessions, put_session
 from flask import Blueprint, flash, g, redirect, render_template, request, session, url_for, g
 from flask_login import login_user, logout_user, login_required, current_user
+from datetime import datetime
 
 from .forms import SessionForm
 from ..models import Session
@@ -23,19 +24,37 @@ def exercise_history():
 # Session routes
 #
 @login_required
-@bp.route('/athlete/session')
+@bp.route('/athlete/session', methods=['GET', 'POST'])
 def sessions():
-    form = SessionForm()
-    if form.validate_on_submit():
-        session = Session()
-        put_session(session, current_user.id)
 
-        return render_template('athlete/sessions.html', current_user=current_user, form=form)
-    else:
-        return render_template('athlete/sessions.html', current_user=current_user, form=form)
+    athlete = get_athlete(current_user.id)
+    all_sessions = get_sessions()
+    athlete_sessions = filter(lambda session: session.key in athlete.sessions, all_sessions)
+
+    return render_template('athlete/sessions.html', athlete_sessions=athlete_sessions, current_user=current_user)
 
 @login_required
-@bp.route('/athlete/session/add', methods=['POST'])
+@bp.route('/athlete/session/add', methods=['GET', 'POST'])
 def session_add():
 
-    return redirect(url_for('athlete.session'))
+    form = SessionForm()
+
+    if form.validate_on_submit():
+
+        exercises = []
+
+        for i in range(5):
+            exercises.append({
+                "Name": form[f'exercises-{i}-exercise'],
+                "Repetitions": form[f'exercises-{i}-repetitions'],
+                "Sets": form[f'exercises-{i}-sets'],
+                "Weight": form[f'exercises-{i}-weight']
+            })
+
+        key = datetime.now().strftime("%c")
+        session = Session(key, request.form['date'], exercises)
+        put_session(session, current_user.id)
+
+        return redirect(url_for('athlete.session'))
+    else:
+        return render_template('athlete/sessions_add.html', current_user=current_user, form=form)
