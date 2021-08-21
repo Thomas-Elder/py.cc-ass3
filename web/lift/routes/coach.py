986e9b0594db4ca36a65161126b841a10098badc
Thomas <thombsaway@gmail.com>
2021-08-21 11:28:39 +1000

    Add session data for athletes for displaying on coach page,.

--- web/lift/routes/coach.py
+++ web/lift/routes/coach.py
@@ -1,7 +1,10 @@
 
 from flask import Blueprint, flash, g, redirect, render_template, request, session, url_for, g
-from ..api import get_athlete, get_coach, get_athletes, put_coach, put_athlete
+from requests import models
+from ..api import get_athlete, get_coach, get_athletes, get_sessions, put_coach, put_athlete
 from flask_login import login_user, logout_user, login_required, current_user
+
+from ..models import Athlete, Coach
 
 bp = Blueprint('coach', __name__)
 
@@ -9,11 +12,16 @@ bp = Blueprint('coach', __name__)
 @bp.route('/coach/athletes', methods=['GET'])
 def athletes():
 
+    all_sessions = get_sessions()
     all_athletes = get_athletes()
     coach = get_coach(current_user.id)
 
-    coach_athletes = filter(lambda athlete: athlete.email in coach.athletes, all_athletes)
-    coachless_athletes = filter(lambda athlete: athlete.coach is None, all_athletes)
+    coach_athletes = list(filter(lambda athlete: athlete.email in coach.athletes, all_athletes))
+    coachless_athletes = list(filter(lambda athlete: athlete.coach is None, all_athletes))
+
+    for a in coach_athletes:
+        athlete_sessions = list(filter(lambda session: session.key in a.sessions, all_sessions))
+        a.sessions = athlete_sessions
 
     return render_template('coach/athletes.html', current_user=current_user, coachless_athletes=coachless_athletes, coach_athletes=coach_athletes)
 
